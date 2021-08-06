b23bcce86ea44b3ce37980796a5573be5793666d
FabioD3V <84205529+FabioD3V@users.noreply.github.com>
2021-08-06 08:53:19 -0500

    Delete CymaxAssessment.API directory

--- CymaxAssessment.API/Models/Carton.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CymaxAssessment.API.Models
-{
-    public class Carton
-    {
-        public int Id { get; set; }
-        public int Dimension { get; set; }
-        public double Price { get; set; }
-    }
-}
